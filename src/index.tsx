bb29a95bc502a35b6c3d2a91c017c1119b7628c4
Veekas Ashoka <3917726+veekas@users.noreply.github.com>
2022-01-27 23:24:03 -0500

    add honeybadger

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
+import Honeybadger from "@honeybadger-io/js";
+import ErrorBoundary from "@honeybadger-io/react";
 import "antd/dist/antd.css";
 
 import Router from "./router";
 import i18n from "./translation";
 
+Honeybadger.configure({
+  apiKey: "hbp_K6fksePV9ks43xiGYJDBxP3N46oTzh2Amunp",
+  environment: "production",
+});
+
 const App = () => (
-  <BrowserRouter>
-    <I18nextProvider i18n={i18n}>
-      <Router />
-    </I18nextProvider>
-  </BrowserRouter>
+  <ErrorBoundary honeybadger={Honeybadger}>
+    <BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+        <Router />
+      </I18nextProvider>
+    </BrowserRouter>
+  </ErrorBoundary>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));

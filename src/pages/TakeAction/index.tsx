9eea8f01abbaadecbffe7bf42f31b3b220fb6822
Veekas Ashoka <3917726+veekas@users.noreply.github.com>
2022-04-10 13:45:56 -0400

    update take action page

--- src/pages/TakeAction/index.tsx
+++ src/pages/TakeAction/index.tsx
@@ -1,6 +1,9 @@
 import { lazy } from "react";
 
 import TakeActionContent from "../../content/TakeActionContent.json";
+import bikeTrek from "../../assets/EarthWeek-BikeTrek.jpg";
+import rally22 from "../../assets/4-22_albany.jpg";
+
 const MiddleBlock = lazy(() => import("../../components/MiddleBlock"));
 const Container = lazy(() => import("../../common/Container"));
 const ScrollToTop = lazy(() => import("../../common/ScrollToTop"));
@@ -36,18 +39,6 @@ const Bills = () => {
                 demand that they pass our package of bills this session because
                 Climate Can’t Wait!
               </p>
-              {/* <p>
-                <b>
-                  <a
-                    href="https://actionnetwork.org/events/climate-cant-wait-earth-day-mobilization"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Sign up here to be kept informed about all the Climate Can’t
-                    Wait rallies!
-                  </a>
-                </b>
-              </p> */}
             </>
           }
           button={TakeActionContent.button}
@@ -57,7 +48,85 @@ const Bills = () => {
       <Container>
         <ContentBlock
           type="left"
-          title="Meanwhile, here's some actions you can take right now!"
+          title="Join us in Albany on April 22nd!"
+          content={
+            <>
+              <div>
+                <b>
+                  Register to attend{" "}
+                  <a
+                    href="https://actionnetwork.org/events/climate-cant-wait-earth-day-mobilization"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    here
+                  </a>
+                  !
+                </b>
+              </div>
+              <br />
+              <div>
+                <div>
+                  <b>Busses</b>
+                </div>
+                <br />
+                Need a ride? Reserve your spot on the bus at one of the
+                following pick-up points:
+                <ul>
+                  <li>
+                    <a href="https://actionnetwork.org/events/manhattan-bus-to-albany">
+                      Manhattan
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://actionnetwork.org/events/brooklyn-bus-to-albany">
+                      Brooklyn
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://actionnetwork.org/events/long-island-queens-bus-to-albany">
+                      Long Island &amp; Queens
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://actionnetwork.org/events/white-plains-new-paltz-bus-to-albany">
+                      White Plains &amp; New Paltz
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://actionnetwork.org/events/buffalo-rochester-bus-to-albany">
+                      Buffalo &amp; Rochester
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </>
+          }
+          image={rally22}
+          section="albany-action"
+          id="albany-action"
+        />
+        <ContentBlock
+          type="right"
+          title="Join a local rally during the week!"
+          content={
+            <>
+              <div>
+                A group of young climate justice activists are riding their
+                bicycles from New York City to Albany to raise awareness about
+                the importance of climate action in New York. Greet them as they
+                ride through your area during the week. See the full schedule{" "}
+                <a href="/bike-trek">here</a>.
+              </div>
+            </>
+          }
+          image={bikeTrek}
+          section="bike-trek-actions"
+          id="bike-trek-actions"
+        />
+        <ContentBlock
+          type="left"
+          title="You can also make a difference anytime you're free"
           content={
             <>
               <div>
@@ -91,6 +160,21 @@ const Bills = () => {
                   easy remote action guide
                 </a>{" "}
                 for opportunities to help in other ways!
+              </div>
+              <br />
+              <div>
+                <div>Stay informed</div>
+                <br />
+                Sign up{" "}
+                <a
+                  href="https://actionnetwork.org/events/climate-cant-wait-earth-day-mobilization"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  here
+                </a>{" "}
+                to be kept informed about all the Climate Can’t Wait 2022 will
+                be working on after Earth Day!
               </div>
             </>
           }
